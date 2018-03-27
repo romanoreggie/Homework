47a8c826f233d4ab13a8e050d675fd2eb5681eca
Reggie Romano <romanoreggie@gmail.com>
2018-03-27 09:27:20 -0500

    Straightening out git hub

---
+++ Week_Two/monday/answers.js
@@ -0,0 +1,85 @@
+// A light switch that can be either on or off.
+// A user's email address.
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// A list of student names from our class.
+// A list of student names from our class, each with a location.
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+// 1.Boolean
+// let lightSwitch = (isOn === true)
+
+// 2.String
+// email = ("romanoreggie@gmail.com")
+
+// 3.String
+// let spaceship = ("hull", "laser blasters", "tractor beam", "warp drive")
+
+// 4.Array
+// let class = ["Susie", "Johny", "Bill", "Frodo"];
+
+// 5.Object and array
+// let class = [
+//   {name: "Susie", location: "Austin"};
+//   {name: "Fred", location: "Dallas"};
+//   {name:"Frodo", location: "Houston"}
+// ];
+
+// 6.Object and array
+// let class = [
+//   {name: "Susie", location: "Austin", shows: ["Greys Anatomy", "Vampire Diaries",]};
+//   {name: "Fred", location: "Dallas", shows: ["Pimp My Ride", "Cribs", "Room Raiders"]};
+//   {name:"Frodo", location: "Houston", shows: ["Rick And Morty", "Archer", "Brickleberry"]}
+// ]
+
+// Make an array that holds all of the colors of the rainbow.
+// Write code that will access "blue" from the rainbow array.
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+// Write code that will access your hobby from the object that you just created.
+
+// 1.let colors = ["Red","Orange","Yellow","Green","Blue","Indigo","Violet"];
+// 2.colors[4]
+//3.let name = {favoriteFood: "Pizza", hobby: "Jiu-Jitsu", town: "Austin" datatype: "Object"};
+//4.name.hobby
+
+// const crazyObject = {
+//   taco: [{meat: 'steak',
+//          cheese: ['panela', 'queso', 'chihuahua']},
+//          {meat: 'chicken',
+//           salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+//         ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [{
+//       name: "Jeff",
+//       occupation: "manager"
+//     },
+//     {
+//       name: "funkhauser",
+//       occupation: "tv dude"
+//     },
+//     {
+//       name: "susie",
+//       occupation: "jeffs wife",
+//       favourtieHobby: "Swearing at Larry and Jeff"
+//     },
+//     ]
+//   }
+// }
+
+// "omg my mouth is burning"
+//
+// "Pretty pretty prettayyyyy good"
+//
+// "Swearing at Larry and Jeff"
+//
+// "Chicken Teriyaki Boyyyyyy"
+//
+// The object the contains the name funkhauser
+
+
+// 1.crazyObject.taco[1].salsa[5]
+// 2.crazyObject.larry.quotes[0]
+// 3.crazyObject.larry.characters[2].favourtieHobby
+// 4.crazyObject.larry.nicknames[1]
+// 5.crazyObject.larry.characters[1].name
