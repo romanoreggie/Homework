d17c2b2e3119a6a4c5fb2c18a0b6fde0824c55ac
Reggie Romano <romanoreggie@gmail.com>
2018-04-02 14:47:37 -0500

    adjusted so can not use blank commit

--- Week_Two/tuesday/app.js
+++ Week_Two/tuesday/app.js
@@ -1,18 +1,23 @@
 console.log("Up and running");
 
-let pastPost = ["hello", "good bye", "nice page", "cool site"];
+let pastPost = [];
 
-document.getElementById("postButton").addEventListener("click", function (e) {
+document.getElementById("postButton").addEventListener("click", function(e) {
 
-e.preventDefault();
+  e.preventDefault();
+if (pastPost == ("")){
+  alert("Must input text");
+} else {
 
-let textPost = document.getElementById('post').value;
-pastPost.push(textPost)
-document.getElementById('text1').innerHTML = pastPost[pastPost.length-1];
-document.getElementById('text2').innerHTML = pastPost[pastPost.length-2];
-document.getElementById('text3').innerHTML = pastPost[pastPost.length-3];
-  document.getElementById('text4').innerHTML = pastPost[pastPost.length-4];
-console.log(pastPost[pastPost.length-1]);});
+  let textPost = document.getElementById('post').value;
+  pastPost.push(textPost)
+  document.getElementById('text1').innerHTML = pastPost[pastPost.length - 1];
+  document.getElementById('text2').innerHTML = pastPost[pastPost.length - 2];
+  document.getElementById('text3').innerHTML = pastPost[pastPost.length - 3];
+  document.getElementById('text4').innerHTML = pastPost[pastPost.length - 4];
+  console.log(pastPost[pastPost.length - 1]);
+}
+});
 
 // document.getElementById("postButton").addEventListener("click", function (e) {
 //   e.preventDefault();
